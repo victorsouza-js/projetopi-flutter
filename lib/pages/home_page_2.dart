8d5c974e0df138ec76d80e9b6991f35b25bd7440
victorsouza-js <victorsouza5611@gmail.com>
2025-05-22 20:27:47 -0300

    add

--- lib/pages/home_page_2.dart
+++ lib/pages/home_page_2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:projeto_pi_flutter/pages/profile_page.dart';
 
 class HomePage2 extends StatefulWidget {
   const HomePage2({super.key});
@@ -8,47 +10,95 @@ class HomePage2 extends StatefulWidget {
 }
 
 class _HomePage2State extends State<HomePage2> {
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            SizedBox(height: 20),
+            Icon(Icons.account_circle, color: Colors.white, size: 100),
+            SizedBox(height: 20),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.white),
+              title: Text('Home', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings, color: Colors.white),
+              title: Text(
+                'Configurações',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.info, color: Colors.white),
+              title: Text('Sobre', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pushNamed(context, '/about');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout, color: Colors.white),
+              title: Text('Sair', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.pop(context); // Fecha o Drawer
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
+        title: Container(
+          height: 40,
+          child: TextField(
+            controller: _searchController,
+            style: TextStyle(color: Colors.orange.shade700),
+            decoration: InputDecoration(
+              hintText: 'Pesquisar',
+              hintStyle: TextStyle(color: Colors.orange.shade700),
+              prefixIcon: Icon(Icons.search, color: Colors.orange.shade700),
+              filled: true,
+              fillColor: Colors.orange.shade700,
+              contentPadding: EdgeInsets.symmetric(vertical: 0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
         actions: [
           Row(
             children: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.signal_cellular_alt)),  // Ícone de sinal
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.signal_cellular_alt),
+              ), // Ícone de sinal
               SizedBox(width: 10),
-              PopupMenuButton<String>(
-                icon: Icon(Icons.wifi, color: Colors.white), // Ícone de Wi-Fi
-                onSelected: (String value) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Conectado à rede: $value')),
-                  );
-                },
-                itemBuilder:
-                    (BuildContext context) => <PopupMenuEntry<String>>[
-                      PopupMenuItem<String>(
-                        value: 'Rede 1',
-                        child: Text('Rede 1'),
-                      ),
-                      PopupMenuItem<String>(
-                        value: 'Rede 2',
-                        child: Text('Rede 2'),
-                      ),
-                      PopupMenuItem<String>(
-                        value: 'Rede 3',
-                        child: Text('Rede 3'),
-                      ),
-                    ],
-              ),
+
               SizedBox(width: 10),
               Icon(Icons.battery_full, color: Colors.white), // Ícone de bateria
               SizedBox(width: 10),
+              Icon(FontAwesomeIcons.cartShopping, color: Colors.white),
+              // Ícone de localização
             ],
           ),
         ],
-        iconTheme: const IconThemeData(color: Colors.white),
-        backgroundColor: Colors.blue,
-        title: const Text('Home Page 2'),
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Colors.orange,
       ),
     );
   }
