40d563e48d438ba7f9576e14c7508927fe983d35
victorsouza-git <victorsouzaaa5611@gmail.com>
2025-05-23 21:17:14 -0300

    add splashscreen

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -3,6 +3,7 @@ import 'package:projeto_pi_flutter/pages/cadastro.dart';
 import 'package:projeto_pi_flutter/pages/home_page.dart';
 import 'package:projeto_pi_flutter/pages/home_page_2.dart';
 import 'package:projeto_pi_flutter/pages/profile_page.dart';
+import 'package:projeto_pi_flutter/splashscreen.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -10,13 +11,15 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
+      initialRoute: '/splash',
       routes: {
         '/home': (context) => HomePage(),
         '/home2': (context) => HomePage2(),
         '/cadastro': (context) => Cadastro(),
         '/profile': (context) => ProfilePage(),
+        '/splash': (context) => SplashScreen(),
 
       },
       );
---
+++ lib/splashscreen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _lottieController;
+  @override
+  void initState() {
+    super.initState();
+    _lottieController = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..forward();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange.shade700,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.network(
+              'https://lottie.host/d24168f4-e0c0-458c-8a7b-50c524716de0/uRiXGAp7La.json',
+              width: 180,
+              height: 180,
+              controller: _lottieController,
+              onLoaded: (composition) {
+                _lottieController.duration = composition.duration;
+                _lottieController.forward();
+              },
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Bem-vindo ao FitXpert',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Seu app de treino e saúde!',
+              style: TextStyle(color: Colors.white70, fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
